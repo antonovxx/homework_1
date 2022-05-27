e256329717b97ced20317de35482e22785650672
Anton Antonov <antonkurin1@gmail.com>
2022-05-27 05:44:21 +0300

    add some tasks

---
+++ hw_1/task_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+
+namespace task_7
+{
+    // Описать структуру Article, содержащую следующие поля: код товара; название товара; цену товара.
+    struct Article
+    {
+        public int id;
+        public string title;
+        public int price;
+
+        public void description()
+        {
+            Console.WriteLine($"Id: {id}\nTitle: {title}\nPrice: {price}");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Article art = new Article();
+
+            art.id = 1;
+            art.title = "Article";
+            art.price = 12312;
+
+            art.description();
+        }
+    }
+}
