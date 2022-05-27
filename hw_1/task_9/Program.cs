d59bce79f7ccbb16bb9644e430e090d03bb4e8ed
Anton Antonov <antonkurin1@gmail.com>
2022-05-27 05:51:52 +0300

    add task 9

--- hw_1/task_9/Program.cs
+++ hw_1/task_9/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Описать структуру RequestItem содержащую поля: товар; количество единиц товара.
+
+namespace task_9
+{
+    struct RequestItem
+    {
+        public string name { get; set; }
+        public int count { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Product name: {name}" +
+                $"\nCount: {count}")
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            RequestItem ri = new RequestItem();
+            ri.name = "Box";
+            ri.count = 15;
+            ri.Print();
+        }
+    }
+}
